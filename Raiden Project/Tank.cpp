c7f50d41cce562e94aa9373149d9165545399742
Skyway666 <lucasgarciamateu@gmail.com>
2017-04-20 18:38:12 +0200

    Advances on the tank sprites

--- Raiden Project/Tank.cpp
+++ Raiden Project/Tank.cpp
@@ -11,8 +11,16 @@ Tank::Tank(int x, int y) : Enemy(x, y)
 {
 	
 	spriteshit = 2;
-	animations = 2;
+	animations = 1;
 	downup.PushBack({ 2, 4, 24, 26 });
+	leftright.PushBack({ 30,5, 30, 16 });
+	neutraldiagonalleft.PushBack({ 61,1,31,30 });
+	downdiagonalleft.PushBack({ 126,2, 30, 29 });
+	updiagonalleft.PushBack({ 157, 4, 31, 25 });
+	neutraldiagonalright.PushBack({ 94, 2, 30, 28});
+	downdiagonalleft.PushBack({222, 3, 30, 28});
+	updiagonalleft.PushBack({ 189,5, 31, 26 });
+
 	w = 24 * 3;
 	h = 26 * 3;
 
@@ -30,5 +38,5 @@ Tank::Tank(int x, int y) : Enemy(x, y)
 void Tank::MoveShoot()
 {
 	position = original_pos + movement.GetCurrentPosition();
-	animation = &downup;
+	animation = &neutraldiagonalright;
 }
--- Raiden Project/Tank.h
+++ Raiden Project/Tank.h
@@ -5,6 +5,13 @@ class Tank: public Enemy
 {
 private:
 	Animation downup;
+	Animation leftright;
+	Animation neutraldiagonalleft;
+	Animation downdiagonalleft;
+	Animation updiagonalleft;
+	Animation neutraldiagonalright;
+	Animation downdiagonalright;
+	Animation updiagonalright;
 	iPoint original_pos;
 	iPoint distance;
 	Path movement;
