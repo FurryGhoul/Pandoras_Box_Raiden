cb5b4f5074a8af32e34ad4ee716d7ed319a2fa22
FurryGhoul <ivandrofiak@hotmail.com>
2017-04-21 17:47:24 +0200

    Animation almost fixed

--- Raiden Project/Enemy_Bonus_Ship.cpp
+++ Raiden Project/Enemy_Bonus_Ship.cpp
@@ -10,24 +10,31 @@
 
 Enemy_Bonus_Ship::Enemy_Bonus_Ship(int x, int y) : Enemy (x, y)
 {
-	flydown.PushBack({ 6, 23, 54, 52 });
+	flydown.PushBack({ 6, 23, 59, 54 });
 
-	stop.PushBack({ 67, 23, 54, 52 });
-	stop.PushBack({ 262, 23, 54, 52 });
-	//stop.speed = 0.04;
+	stop.PushBack({ 67, 23, 59, 54 });
+	stop.PushBack({ 262, 23, 59, 54 });
 
 	flysides.PushBack({ 131, 23, 59, 54 });
 	flysides.PushBack({ 197, 23, 59, 54 });
-	//flysides.speed = 0.2f;
-	//flysides.loop = true;
+	flysides.speed = 0.5f;
+	flysides.loop = true;
+
+	none.PushBack({ 500, 500, 32, 14 });
 
 	propeller.PushBack({ 143, 83, 32, 14 });
 	propeller.PushBack({ 194, 83, 32, 14 });
 	propeller.speed = 0.5f;
 	propeller.loop = true;
 
+	charge.PushBack({ 326, 23, 59, 54 });
+	charge.PushBack({ 392, 23, 59, 54 });
+	charge.PushBack({ 457, 23, 59, 54 });
+	charge.speed = 2.0f;
+	charge.loop = true;
+
 	movement.PushBack({ 0.0f, 1.5f }, 300);
-	movement.PushBack({ 0.0f, 0.0f }, 50);
+	movement.PushBack({ 0.0f, 0.0f }, 100);
 	movement.PushBack({ -0.5f, 0.0f }, 300);
 	movement.PushBack({ 0.5f, 0.0f }, 600);
 	movement.PushBack({ -0.5f, 0.0f }, 600);
@@ -37,32 +44,42 @@ Enemy_Bonus_Ship::Enemy_Bonus_Ship(int x, int y) : Enemy (x, y)
 
 	collider = App->collision->AddCollider({ 0, 0, 60, 60 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 	spriteshit = 1;
-	animations = 2;
+	animations = 3;
 	original_pos.x = x;
 	original_pos.y = - 200;
-	original_pos1.x = + 455;
-	original_pos1.y = - 236;
+	original_pos1.x = + 457;
+	original_pos1.y = - 238;
+	original_pos2.x = x + 4;
+	original_pos2.y = -200;
 }
 
 void Enemy_Bonus_Ship::MoveShoot()
 {
 	position = original_pos + movement.GetCurrentPosition();
 	position1 = original_pos1 + movement.GetCurrentPosition();
-	w = 54 * 3;
-	h = 52 * 3;
+	position2 = original_pos2 + movement.GetCurrentPosition();
+	w = 59 * 3;
+	h = 54 * 3;
 	w1 = 32 * 3;
 	h1 = 14 * 3;
+	w2 = 59 * 3;
+	h2 = 54 * 3;
 	if (position.x < 410 || position.x > 410)
 	{
 		animation = &flysides;
+		animation1 = &none;
+		animation2 = &charge;
 		side = true;
 	}
 	if (position.y > 234 && side == false)
 	{
 		animation = &stop;
+		animation1 = &none;
+		animation2 = &none;
 	}
-	if (position.y <= 234 && position1.y <= 270)
+	if (position.y <= 234)
 	{
+		animation2 = &none;
 		animation1 = &propeller;
 		animation = &flydown;
 	}
