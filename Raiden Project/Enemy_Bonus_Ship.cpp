b9013c935e1d6906beeb03e5dfe3078d8dbb3d89
Skyway666 <lucasgarciamateu@gmail.com>
2017-04-23 00:15:59 +0200

    Well merged

--- Raiden Project/Enemy_Bonus_Ship.cpp
+++ Raiden Project/Enemy_Bonus_Ship.cpp
@@ -69,17 +69,11 @@ void Enemy_Bonus_Ship::MoveShoot()
 {
 	position = original_pos + movement.GetCurrentPosition();
 	position1 = original_pos1 + movement.GetCurrentPosition();
-<<<<<<< HEAD
-	//position2 = original_pos2 + movement.GetCurrentPosition();
-	
-	
-
-=======
 	position2 = original_pos2 + movement.GetCurrentPosition();
 	position.x += left_right_mod;
 	position1.x += left_right_mod;
 	position2.x += left_right_mod;
->>>>>>> origin/master
+
 	w = 59 * 3;
 	h = 54 * 3;
 	w1 = 32 * 3;
