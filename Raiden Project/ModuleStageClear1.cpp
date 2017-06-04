03d35100ca6a4a0976610b0e379ec883a95f5c7b
retsnom9 <retsnom797@gmail.com>
2017-06-04 04:02:15 +0200

    Winning screen

--- Raiden Project/ModuleStageClear1.cpp
+++ Raiden Project/ModuleStageClear1.cpp
@@ -12,6 +12,9 @@
 #include "ModulePlayer.h"
 #include "ModulePlayer2.h"
 #include "ModuleShadows.h"
+#include "ModuleFonts.h"
+
+#include <stdio.h>
 
 ModuleStageClear1::ModuleStageClear1() : Module()
 {
@@ -26,7 +29,7 @@ bool ModuleStageClear1::Init()
 {
 	LOG("Loading background assets");
 	ground = { 0, 0 ,224 * 3 ,256 * 3 };
-	StageClear1 = App->textures->Load("Assets/StageClear1.png");
+	StageClear1 = App->textures->Load("Assets/Winningscreen.png");
 	return true;
 }
 
@@ -40,11 +43,24 @@ update_status ModuleStageClear1::Update()
 		App->player2->Disable();
 		App->collision->Disable();
 	}
+
 	App->render->Blit(StageClear1, 0, 0, &ground, 224 * 3, 256 * 3+ 62);
+	sprintf_s(text, 60, "CONGRATULATIONS!YOU BEAT");
+	sprintf_s(text1, 60, "OUR GAME!THANK YOU FOR");
+	sprintf_s(text2, 60, "PLAYING IT,YOUR HIGHEST");
+	sprintf_s(text3, 60, "SCORE WAS:");
+
+	App->fonts->BlitText(50, 600, 1, text);
+	App->fonts->BlitText(50, 630, 1, text1);
+	App->fonts->BlitText(50, 660, 1, text2);
+	App->fonts->BlitText(50, 690, 1, text3);
+	App->fonts->BlitText(300, 690, 0, App->player->highscoret);
+
 	if ((App->input->keyboard[SDL_SCANCODE_SPACE] && !App->input->gpad) || (App->input->gamepad[6] && App->input->gpad))
 	{
 		App->fade->FadeToBlack(this, App->WelcomeScreen);
 	}
+
 	return UPDATE_CONTINUE;
 }
 
--- Raiden Project/ModuleStageClear1.h
+++ Raiden Project/ModuleStageClear1.h
@@ -22,6 +22,11 @@ public:
 	SDL_Texture *StageClear1;
 	int xmap = 0;
 	int ymap = 0;
+	char text[60];
+	char text1[60];
+	char text2[60];
+	char text3[60];
+	char text4[60];
 };
 
 #endif //__MODULESTAGECLEAR__
